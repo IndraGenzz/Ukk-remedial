13e9d35a64282be51c16494dd3647129237b9ffc
Indra_Prawangsa <indraprawangsa51@gmail.com>
2025-11-19 21:50:34 +0700

    Add files via upload

---
+++ Remidi-ukk/index.php
@@ -0,0 +1,67 @@
+<?php include 'koneksi.php'; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Data Alumni</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h2>Data Alumni Sekolah</h2>
+    <a href="tambah.php" id="tambahdata">+ Tambah Data</a>
+
+    <!-- FORM PENCARIAN -->
+    <form method="GET" style="margin-bottom: 20px;">
+        <input type="text" name="cari" placeholder="Cari Nama Lengkap / Tahun Lulus / Jurusan / Pekerjaan Saat Ini..."
+            value="<?= isset($_GET['cari']) ? $_GET['cari'] : '' ?>">
+        <button type="submit">Cari</button>
+    </form>
+
+    <table>
+        <tr>
+            <th>id_alumni</th>
+            <th>Nama_Lengkap</th>
+            <th>tahun_lulus</th>
+            <th>Jurusan</th>
+            <th>Pekerjaan_Saat_Ini</th>
+            <th>Nomor_Telepon</th>
+            <th>Email</th>
+            <th>Alamat</th>
+            
+        </tr>
+
+        <?php
+        // PENCARIAN
+        if (isset($_GET['cari'])) {
+            $cari = $_GET['cari'];
+            $result = mysqli_query($conn, "SELECT * FROM alumni 
+                WHERE id_alumni LIKE '%$cari%'
+                OR Tahun_lulus LIKE '%$cari%' 
+                OR Jurusan LIKE '%$cari%' 
+                OR Pekerjaan_Saat_Ini LIKE '%$cari%' ");
+        } else {
+            $result = mysqli_query($conn, "SELECT * FROM alumni");
+        }
+
+        // TAMPILKAN DATA
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>
+                <td>{$row['Nama_Lengkap']}</td>
+                <td>{$row['Tahun_lulus']}</td>
+                <td>{$row['Jurusan']}</td>
+                <td>{$row['Pekerjaan_Saat_Ini']}</td>
+                <td>{$row['Nomor_Telepon']}</td>
+                <td>{$row['Email']}</td>
+                <td>{$row['Alamat']}</td>
+                <td>
+                    <a href='edit.php?id={$row['id_alumni']}'>Edit</a> |
+                    <a href='hapus.php?id={$row['id_alumni']}' onclick=\"return confirm('Yakin ingin hapus?')\">Hapus</a>
+                </td>
+            </tr>";
+        }
+        ?>
+    </table>
+</body>
+
+</html>
---
+++ Remidi-ukk/tambah.php
@@ -0,0 +1,35 @@
+<?php include 'koneksi.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h2>Tambah alumni</h2>
+    <form method="POST">
+        <input type="text" name="Nama_Lengkap" placeholder="Nama" required>
+        <input type="number" name="Tahun_Lulus" placeholder="Tahunlulus" required>
+        <input type="text" name="Jurusan" placeholder="jurusan" required>
+        <input type="text" name="Pekerjaan_Saat_Ini" placeholder="Pekerjaan Saat Ini" required>
+        <input type="number" name="Nomor_Telepon" placeholder="no.telpon" required>
+        <input type="text" name="Email" placeholder="email" required>
+        <textarea name="Alamat" placeholder="Alamat" required></textarea>
+        <button type="submit" name="submit">Simpan</button
+        </form>
+
+        <?php
+        if (isset($_POST['submit'])) {
+            $sql = "INSERT INTO alumni (Nama_Lengkap,Tahun_Lulus,Jurusan,Pekerjaan_Saat_Ini,Nomor_Telepon,Email,Alamat)
+    VALUES ('$_POST[Nama_Lengkap]','$_POST[Tahun_Lulus]','$_POST[Jurusan]','$_POST[Pekerjaan_Saat_Ini]','$_POST[Nomor_Telepon]','$_POST[Email]','$_POST[Alamat]')";
+            mysqli_query($conn, $sql);
+            echo "<p>Data berhasil disimpan! <a href='index.php'>Kembali</a></p>";
+        }
+        ?>
+</body>
+
+</html>
